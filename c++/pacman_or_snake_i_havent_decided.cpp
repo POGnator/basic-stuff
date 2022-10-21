7e39a3a7835866efa5b8b0645ddc611f9ae205c7
POGnator <stammlergabriel@gmail.com>
2022-10-21 17:39:42 +0200

    Working goal

--- c++/pacman_or_snake_i_havent_decided.cpp
+++ c++/pacman_or_snake_i_havent_decided.cpp
@@ -2,19 +2,24 @@
 #include <windows.h>
 #include <cmath>
 #include <math.h>
+#include <time.h>
 using namespace std;
 //The "map" that we're going to use
-string map = "..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........";
+string map = "O.........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........";
 
 int calcPos(int xpos, int ypos){
+    //Don't ask what I did here
     return(xpos + 10*(sqrt(pow(ypos, 2)))-ypos);
 };
+
 int main(){
+    srand(time(0));
     system("cls");
     string answer;
     int Xplayer = 0;
     int Yplayer = 0;
     cout << "Started" << endl;
+    map[calcPos(rand() % 9, rand() % -9)] = 'X';
     while (answer != "e")
     {
         system("cls");
@@ -36,10 +41,12 @@ int main(){
             map[calcPos(Xplayer, Yplayer)] = '.';
             Xplayer += 1;
         };
+        if (map[calcPos(Xplayer, Yplayer)] == 'X'){
+            cout << "You win!";
+            answer = "e";
+        };
         map[calcPos(Xplayer, Yplayer)] = 'O';
         
     }
-    
-    
     return 0;
 }
