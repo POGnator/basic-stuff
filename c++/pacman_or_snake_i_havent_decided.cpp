433a2d86606d0ae77ce570023242ac090b6f33e1
POGnator <stammlergabriel@gmail.com>
2022-10-22 20:28:54 +0200

    Collision detection

--- c++/pacman_or_snake_i_havent_decided.cpp
+++ c++/pacman_or_snake_i_havent_decided.cpp
@@ -5,7 +5,7 @@
 #include <time.h>
 using namespace std;
 //The "map" that we're going to use
-string map = "O.........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........";
+string map = "O.........\n..........\n..........\n......*...\n......*...\n..........\n..........\n..........\n..........\n..........";
 
 int calcPos(int xpos, int ypos){
     //Don't ask what I did here
@@ -25,19 +25,19 @@ int main(){
         system("cls");
         cout << map << endl << "Player X: " << Xplayer << endl << "Player Y: " << Yplayer << endl;
         cin >> answer;
-        if (answer == "w" && Yplayer < 0){
+        if (answer == "w" && Yplayer < 0 && map[calcPos(Xplayer, Yplayer+1)] != '*'){
             map[calcPos(Xplayer, Yplayer)] = '.';
             Yplayer += 1;
         }
-        if (answer == "s" && Yplayer > -9){
+        if (answer == "s" && Yplayer > -9 && map[calcPos(Xplayer, Yplayer-1)] != '*'){
             map[calcPos(Xplayer, Yplayer)] = '.';
             Yplayer -= 1;
         }
-        if (answer == "a" && Xplayer > 0){ 
+        if (answer == "a" && Xplayer > 0 && map[calcPos(Xplayer-1, Yplayer)] != '*'){ 
             map[calcPos(Xplayer, Yplayer)] = '.';
             Xplayer -= 1;
         }
-        if (answer == "d" && Xplayer < 9){
+        if (answer == "d" && Xplayer < 9 && map[calcPos(Xplayer+1, Yplayer)] != '*'){
             map[calcPos(Xplayer, Yplayer)] = '.';
             Xplayer += 1;
         };
